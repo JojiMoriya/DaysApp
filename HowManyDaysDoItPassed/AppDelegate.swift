5d47354cad07ea12726653fa44b545177a8fc849
Joji Moriya <moriya.joji@gmail.com>
2020-11-16 21:33:33 +0900

    通知機能の下準備実装

--- HowManyDaysDoItPassed/AppDelegate.swift
+++ HowManyDaysDoItPassed/AppDelegate.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: [.alert, .sound, .badge]){
+            (granted, _) in
+            if granted{
+                UNUserNotificationCenter.current().delegate = self
+            }
+        }
 
         return true
     }
@@ -31,7 +40,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    func userNotificationCenter(
+            _ center: UNUserNotificationCenter,
+            willPresent notification: UNNotification,
+            withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            if #available(iOS 14.0, *) {
+                completionHandler([[.banner, .list, .sound]])
+            } else {
+                completionHandler([[.alert, .sound]])
+            }
+        }
 }
+
 
