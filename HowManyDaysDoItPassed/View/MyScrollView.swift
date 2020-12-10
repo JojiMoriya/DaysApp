20ab249a0af2e2e39e12b18cda43ec03e54f36b4
Joji Moriya <moriya.joji@gmail.com>
2020-12-10 23:24:05 +0900

    ListVCのオートレイアウトを設定

---
+++ HowManyDaysDoItPassed/View/MyScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  MyScrollView.swift
+//  HowManyDaysDoItPassed
+//
+//  Created by 守屋譲司 on 2020/12/10.
+//
+
+import UIKit
+
+class MyScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
