8e292bc2a0fd19b75badb57657d35fa6d7c67403
Joji Moriya <moriya.joji@gmail.com>
2020-12-11 17:21:34 +0900

    touchesBeganメソッドをスクロールビュー上でも行えるようScrollVeiwクラスを作成

--- HowManyDaysDoItPassed/View/MyScrollView.swift
+++ HowManyDaysDoItPassed/View/MyScrollView.swift
@@ -2,7 +2,7 @@
 //  MyScrollView.swift
 //  HowManyDaysDoItPassed
 //
-//  Created by 守屋譲司 on 2020/12/10.
+//  Created by 守屋譲司 on 2020/12/11.
 //
 
 import UIKit
@@ -11,4 +11,5 @@ class MyScrollView: UIScrollView {
     override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.superview?.touchesBegan(touches, with: event)
     }
+
 }
